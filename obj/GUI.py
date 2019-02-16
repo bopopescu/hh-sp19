d681bba7791f99241e8a8ab2ddbb915daaef4a39
Spencer R Loggia <spencer.loggia98@gmail.com>
2019-02-16 14:44:10 -0500

    test

--- obj/GUI.py
+++ obj/GUI.py
@@ -1,13 +1,13 @@
-# from tkinter import *
-# import sqlite3 as sq
-#
-# class GUI:
-#     top = Tk()
-#     # BEGIN GUI BOX.
-#
-#     text = Text(top)
-#     text.insert(INSERT, "by Spencer, Kaushik, and Stephanie")
-#     text.pack()
-#
-#     # END GUI BOX
-#     top.mainloop()
+from tkinter import *
+import sqlite3 as sq
+
+class GUI:
+    top = Tk()
+    # BEGIN GUI BOX.
+
+    text = Text(top)
+    text.insert(INSERT, "by Spencer, Kaushik, and Stephanie")
+    text.pack()
+
+    # END GUI BOX
+    top.mainloop()
